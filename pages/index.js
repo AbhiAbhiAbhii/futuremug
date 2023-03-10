46d0b2e7e053d07ad4564828eeb8e29c282a8f8e
AbhiAbhiAbhii <abhilash@gradical.xyz>
2023-03-10 23:17:31 +0530

    push test with HEAD tag

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Hero from '../components/Hero/Hero'
 import SectionTwo from '../components/SectionTwo/SectionTwo'
 import SectionThree from '../components/SectionThree/SectionThree'
@@ -10,6 +11,12 @@ export default function Home({mobileCheck}) {
 
   return (
     <>
+      <Head>
+        <title>FutureMug</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Hero mobileCheck={mobileCheck}/>
       <SectionTwo mobileCheck={mobileCheck} />
       <SectionThree mobileCheck={mobileCheck} />
